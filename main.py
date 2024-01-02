631ebd8e34acded571c1cc04fcb0460db543b574
Gabrielle Ford <gabrielledeva@gmail.com>
2024-01-02 16:03:07 -0500

    setting up the basic/global information

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 # College life sim
+seasons_dict = {1: 'Autumn', 2: 'Winter', 3: 'Spring', 4: 'Summer'}
+grade_levels_dict = {1: 'Freshman', 2: 'Sophomore', 3: 'Junior', 4: 'Senior'}
+locations_list = ['Auditorium', 'Bar', 'Basketball Court', 'Bookstore', 'Cafeteria', 'Classroom', 'Clothing store', 'Club', 'Dorms', 'Football Field', 'Grocery Store', 'Gym', 'Hallway', 'Library', 'Lab', 'Laundry Room', 'Restaurant', 'Stadium', 'Stairwell', 'Quad']
+school_reputation = []
+
+class School:
+  season = seasons[0]
+  day = 1
+  def __init__(self, school_name):
+    self.name = school_name
+
+class Location:
+  def __init__(self):
+    pass
+
+class Student:
+  def __init__(self):
+    pass
+
