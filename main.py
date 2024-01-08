8f0c5c62fe4e3158df015b6b30deca39c37dfbfd
Gabrielle Ford <gabrielledeva@gmail.com>
2024-01-08 14:36:49 -0500

    building out classes

--- main.py
+++ main.py
@@ -10,112 +10,124 @@ locations_list = ['Auditorium', 'Bar', 'Basketball Court', 'Bookstore', 'Cafeter
 majors = ['Architecture', 'Art', 'Biology', 'Business', 'Chemistry', 'Computer Science', 'Criminology', 'Education', 'Economics', 'Engineering', 'Health', 'History', 'Mathematics', 'Sociology', 'Writing']
 
 class School:
-  season = seasons_dict[1]
-  day = 1
-  def __init__(self, school_name):
-    self.name = school_name
+  def __init__(self):
+    self.name = 'Oxbridge University'
+    self.season = seasons_dict[1]
+    self.day = 1
 
 class Location:
   def __init__(self):
     pass
 
 class Student:
-  def __init__(self):
-    self.name = ''
-
-class Player:
-  def __init__(self, name):
+  def __init__(self, name, courses = None, friends = None, enemies = None, romantic_interest = None):
     self.name = name
+    self.courses = courses
+    self.friends = friends
+    self.enemies = enemies
+    self.romantic_interest = romantic_interest
 
-def load_save(player:Player):
-  with open('save_file.json', 'r') as save:
-    data = json.load(save)
-    print(data)
+class Player(Student):
+  def __init__(self, name):
+    super().__init__(name)
 
-def create_player(player:Player):
-  confirmed = False
-  redo = False
-  while not confirmed:
-    if redo == True:
-      player_name = input('I\'m sorry about that, please enter your name again.\n>>> ')
-    else: 
-      player_name = input('Hi, and welcome to your first day at Oxbridge University! What\'s your name?\n>>> ')
-    while True:
-      confirm = input(f'I have you down as {player_name}. Is that correct?\n(y/n)>>> ')
-      if confirm in y_n_resp:
-        if confirm == 'y':
-          player.name = player_name
-          print(f'Welcome, {player.name}!')
-          confirmed = True
-          break
-        elif confirm == 'n':
-          redo = True
-          break
-      print('That\'s an invalid input, please try again.')
+  
 
-  player.name = player_name
+player = Player('Brielle')
+print(player.name)
 
-  save_data = {
-    'player.name': player.name,
-    'School.season': School.season,
-    'School.day': School.day
-  }
+# def load_save(player:Player):
+#   with open('save_file.json', 'r') as save:
+#     data = json.load(save)
+#     player.name = data['player.name']
+#     School.season = data['School.season']
+#     School.day = data['School.day']
+    
 
-  with open('save_file.json', 'w') as save:
-    json.dump(save_data, save)
+# def create_player(player:Player):
+#   confirmed = False
+#   redo = False
+#   while not confirmed:
+#     if redo == True:
+#       player_name = input('I\'m sorry about that, please enter your name again.\n>>> ')
+#     else: 
+#       player_name = input('Hi, and welcome to your first day at Oxbridge University! What\'s your name?\n>>> ')
+#     while True:
+#       confirm = input(f'I have you down as {player_name}. Is that correct?\n(y/n)>>> ')
+#       if confirm in y_n_resp:
+#         if confirm == 'y':
+#           player.name = player_name
+#           print(f'Welcome, {player.name}!')
+#           confirmed = True
+#           break
+#         elif confirm == 'n':
+#           redo = True
+#           break
+#       print('That\'s an invalid input, please try again.')
 
-def launch():
-  if os.path.exists('save_file.json') == True:
-    player = Player('')
-    print(
-      start_screen_title,
-      start_new_or_load
-    )
-    start_input = ''
-    confirmed = False
-    while not confirmed:
-      while True:
-        start_input = input('Type \'1\' and press enter to start a new story at Oxbridge University\nor type \'2\' to continue your previous story.\n>>> ')
-        if start_input == '1':
-          while True:
-            confirm = input('Please confirm that you want to START OVER\n(y/n)>>> ')
-            if confirm in y_n_resp:
-              if confirm == 'y':
-                os.remove('save_file.json')
-                confirmed = True
-                create_player(player)
-                break
-              elif confirm == 'n':
-                break
-              print('That\'s an invalid input, please try again.')
-          break
-        elif start_input == '2':
-          while True:
-            confirm = input('Please confirm that you want to LOAD SAVE\n(y/n)>>> ')
-            if confirm in y_n_resp:
-              if confirm == 'y':
-                confirmed = True
-                load_save(player)
-                break
-              elif confirm == 'n':
-                break
-            print('That\'s an invalid input, please try again.')
-          break
-        print('That\'s an invalid input, please try again.')
-  else:
-    player = Player('')
-    print(
-      start_screen_title,
-      start_new,
-      intro
-    )
-    start_input = None
-    while True:
-      start_input = input('Type \'1\' and press enter to start your life at Oxbridge University!\n>>> ')
-      if start_input == '1':
-        create_player(player)
-        break
-      print('That\'s an invalid input, please try again.')
+#   player.name = player_name
 
-if __name__ == "__main__":
-  launch()
+#   save_data = {
+#     'player_name': player.name,
+#     'School_season': School.season,
+#     'School_day': School.day
+#   }
+
+#   with open('save_file.json', 'w') as save:
+#     json.dump(save_data, save)
+
+# def launch():
+#   if os.path.exists('save_file.json') == True:
+#     player = Player('')
+#     print(
+#       start_screen_title,
+#       start_new_or_load
+#     )
+#     start_input = ''
+#     confirmed = False
+#     while not confirmed:
+#       while True:
+#         start_input = input('Type \'1\' and press enter to start a new story at Oxbridge University\nor type \'2\' to continue your previous story.\n>>> ')
+#         if start_input == '1':
+#           while True:
+#             confirm = input('Please confirm that you want to START OVER\n(y/n)>>> ')
+#             if confirm in y_n_resp:
+#               if confirm == 'y':
+#                 os.remove('save_file.json')
+#                 confirmed = True
+#                 create_player(player)
+#                 break
+#               elif confirm == 'n':
+#                 break
+#               print('That\'s an invalid input, please try again.')
+#           break
+#         elif start_input == '2':
+#           while True:
+#             confirm = input('Please confirm that you want to LOAD SAVE\n(y/n)>>> ')
+#             if confirm in y_n_resp:
+#               if confirm == 'y':
+#                 confirmed = True
+#                 load_save(player)
+#                 break
+#               elif confirm == 'n':
+#                 break
+#             print('That\'s an invalid input, please try again.')
+#           break
+#         print('That\'s an invalid input, please try again.')
+#   else:
+#     player = Player('')
+#     print(
+#       start_screen_title,
+#       start_new,
+#       intro
+#     )
+#     start_input = None
+#     while True:
+#       start_input = input('Type \'1\' and press enter to start your life at Oxbridge University!\n>>> ')
+#       if start_input == '1':
+#         create_player(player)
+#         break
+#       print('That\'s an invalid input, please try again.')
+
+# if __name__ == "__main__":
+#   launch()
